51bc750e75c8e7c8b0e654deeafdeb58073e8257
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-27 14:36:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 //your code here!
+const list = document.getElementById("infi-list");
+
+let count = 1;
+
+// ---- 10 default items add ----
+function addInitialItems() {
+  for (let i = 0; i < 10; i++) {
+    const li = document.createElement("li");
+    li.textContent = "Item " + count++;
+    list.appendChild(li);
+  }
+}
+addInitialItems();
+
+// ---- 2 new items add function ----
+function addMoreItems() {
+  for (let i = 0; i < 2; i++) {
+    const li = document.createElement("li");
+    li.textContent = "Item " + count++;
+    list.appendChild(li);
+  }
+}
+
+// ---- Infinite Scroll Logic ----
+list.addEventListener("scroll", function () {
+  // Check if user reached the bottom
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight-5) {
+    addMoreItems();            // yaha -5 isliye kiye kyuki scrollHeight bara ho raha tha and function call nhi pa rha tha
+  }
+});
 
